ddfaffd93f5b6edfceb4b826fc0b27131e27cf36
Chris Jackson <chrisj@graphcore.ai>
2022-10-02 14:39:08 +0100

    Initial sinister commit
    - Add top-level prpoject.
    - Add skeleton CMakeLists and source files.

---
+++ sinister/lib/sinister/sinister.cpp
@@ -0,0 +1,19 @@
+//===-- sinister.cpp ----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines functions that can be used for parallel code generation.
+//
+//===----------------------------------------------------------------------===//
+#include "sinister/sinister.h"
+#include "lldb/API/SBCommandInterpreter.h"
+
+auto main(int argc, const char **argv) -> int {
+
+
+    return 0;
+}
